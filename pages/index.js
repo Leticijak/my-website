fb326299afa5bce4ad057badc8978ad4c0292116
Leticijak <mnogomijeheavy@gmail.com>
2021-09-21 12:27:41 +0200

    ok

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
     <>
       <Head>
         <title>Ben Tijanic | Home</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
