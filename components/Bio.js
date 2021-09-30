5a6d187111477f414c1c27675fbc23f93f24cd77
Leticijak <mnogomijeheavy@gmail.com>
2021-09-30 18:49:38 +0200

    final

--- components/Bio.js
+++ components/Bio.js
@@ -11,17 +11,18 @@ export default function Bio() {
             src="/photo3.webp"
             width={150}
             height={199}
-            alt="Profile picture Ben Tijanic"
+            alt="profile picture of Ben Tijanic"
           />
         </div>
+
         <div className={styles.right}>
           <p>
             I am a web developer based in Barcelona, Spain. I started my
             programming journey in 2016 with Python3. At the end of 2017, I
-            transitioned into web development and building web apps. I’ve been
+            transitioned into web development and building web apps. I have been
             using JavaScript and its frameworks ever since. Making web sites for
             individuals, local businesses and established brands. Technologies
-            that I'm most proficient at are
+            that I am most proficient at are
           </p>
           <p>
             <span>Front-end</span> : Next,js, React.js, Html5 and Css3, Vanilla
